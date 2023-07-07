97b82eebc8ab03573125e254d7760afb4b0b4b57
Felipe <feliosphere@gmail.com>
2023-07-07 19:41:45 -0400

    Use Pandas and Numpy to calculate monhtly payments on each loan

--- main.py
+++ main.py
@@ -1,41 +1,34 @@
-# import pandas as pd
-import json
+import pandas as pd
+import numpy as np
 
-# df = pd.read_json('creditbalances.json')
+def MonthsToPay(dueDate, requestDate):
+    return 12*(dueDate.dt.year-requestDate.dt.year)+(dueDate.dt.month-requestDate.dt.month)-1
 
-# print(df)[]
-# someDict = {'s1':[{'sub11':1,'sub12':2},{'sub13':3,'sub14':4}],'s2':[{'sub21':5,'sub22':6},{'sub23':7,'sub24':8}]}
-# print(someDict)
-f = open('creditbalances.json')
+def TotalPayment(months, total, apr):
+    total *= ((1+apr/1200)**months)
+    return total
 
-creditCardLoans = json.load(f)
-print(creditCardLoans)
-creditCards = ['Amex', 'Discover', 'ChaseSapphire','MicroCenter']
-loansBalances = {}
-# print()
-for card in creditCardLoans:
-    # print(card,creditCardLoans[card])
-    for loan in creditCardLoans[card]:
-        print(loan['id'])
+df = pd.read_json('creditbalancesflat.json')
 
-        #Loan fee: add to balance of loan or total credit card balance to be paid on first month?
-        loanFee = loan['balance'] * loan['loanFee']/100
-        print(loanFee)
+df['submittedDate'] = pd.to_datetime(df['submittedDate'], format='%Y%m%d')
+df['dueDate'] = pd.to_datetime(df['dueDate'], format='%Y%m%d')
+df['totalLoanDue']  = (df['loanFee']*df['balance']/100+df['balance'])
 
-        #Calculate months (set as function later and check a better way using day and when cycle closes)
-        requestYear = int(loan['submittedDate']/10000)
-        requestMonth = int((loan['submittedDate']-requestYear*10000)/100)
-        requestDay = int(loan['submittedDate']-requestYear*10000-requestMonth*100)
-        dueYear = int(loan['dueDate']/10000)
-        dueMonth = int((loan['dueDate']-dueYear*10000)/100) - 1
-        dueDay = int(loan['dueDate']-dueYear*10000-dueMonth*100)
-        monthsToPay = (dueYear-requestYear)*12 + (dueMonth-requestMonth)
-        print(dueYear,requestYear,dueMonth,requestMonth)
-        print(monthsToPay)
+df['monthsToPay'] = MonthsToPay(df['dueDate'],df['submittedDate'])
 
-        #Assume same interest apply to originator fee (change later if different)
-        totalLoanDue = loan['balance']+loanFee
-        #Append to dict
-        # loansBalances[]
+firstMonth = df['submittedDate'].min()
+lastMonth = df['dueDate'].max()
 
-f.close
+df['totalPayment'] = TotalPayment(df['monthsToPay'],df['totalLoanDue'],df['apr'])
+
+for single_date in pd.date_range(start=firstMonth, end=lastMonth,freq='MS'):
+
+    conditions = [(df['submittedDate'] > single_date),
+                  (df['submittedDate'] < single_date) & (single_date <= df['dueDate']),
+                  (df['submittedDate'] > single_date) | (single_date >= df['dueDate'])]
+    values = [0,df['totalPayment']/df['monthsToPay'],'paid']
+                  
+    df[single_date.strftime('%Y-%m')] = np.select(conditions,values) 
+
+print(df)
+print(df['totalPayment'])
